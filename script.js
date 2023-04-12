d87eb487853ab867b430d365c259d5eebb9d7741
Bhavuk Sagar <bhavuk@Bhavuks-MacBook-Air.local>
2023-04-12 08:33:04 +0530

    added more fields

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Logs")
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("graph");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
